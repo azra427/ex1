fd55e5639f10b2d47a72a4bad588ea638b2a6a7b
azra427 <azrafathima1212@gmail.com>
2025-03-24 12:30:00 +0530

    1.0.0.0

---
+++ app.js
@@ -0,0 +1,145 @@
+
+const Express= require("express");
+
+const BodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const bcrypt=require("bcryptjs");
+const app = Express();
+
+const PORT= 5000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/college"); 
+
+const signUpSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const User= mongoose.model("user",signUpSchema);
+
+app.use(BodyParser.urlencoded({extended:false}));
+
+app.set("view engine","ejs");
+
+app.use(Express.json());
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+
+
+app.get("/about",(req,res)=>{
+    res.send("<h1>this is about page </h1>");
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("<h1>this is contact page </h1>");
+})
+
+app.get("/SignUp",(req,res)=>{
+    res.render("form");
+});
+
+app.post("/SignUp",async(req,res)=>{
+    const Email=req.body.email;
+    const Password=req.body.password;
+
+    if(!Email||!Password){
+        res.status(500).send("<h1>please enter the fields</h1>");
+
+    }
+    //creating salt
+    const salt=await bcrypt.genSalt(10);
+    const hash=await bcrypt.hash(Password,salt);
+    const newUser= new User({
+        email:Email,
+        password:hash
+        //password:Password
+    });
+    newUser.save(); 
+    res.send("<h1>signup successful</h1>");
+})
+
+app.post("/login",async(req,res)=>{
+    const Email=req.body.email;
+    const Password=req.body.password;
+
+    if(!Email||!Password){
+        res.status(500).send("<h1>please enter the fields</h1>");
+
+    }
+    const userDetail=await User.findOne({"email":Email});
+
+    if(!userDetail){
+        res.status(500).send("<h1>please signup</h1>");
+    }
+    // if(userDetail.password !== Password){
+    //     res.status(500).send("<h1>password is incorrect</h1>");
+    // }
+    const comparePass=await bcrypt.compare(Password,userDetail.password);
+    if(!comparePass){
+        res.status(500).send("<h1>password incorrect</h1>");
+    }
+    res.send("logged in");
+    
+})
+
+app.get("/forgotPassword",(req,res)=>{
+     res.render("forgotPassword");
+})
+app.post("/forgotPassword",async(req,res)=>{
+    const Email=req.body.uEmail;
+    const Password=req.body.uPassword;
+
+    const data=await User.findOne({"email":Email});
+    if(!data){
+        res.status(500).send("<h1>please signup</h1>");
+    }  
+    const salt=await bcrypt.genSalt(10);
+    const hash=await bcrypt.hash(Password,salt);
+   await User.updateOne({"email":data.email},{"password":hash});
+
+   res.send({"message":"update success"});
+
+
+})
+    
+
+
+app.listen(PORT,()=>{
+    console.log(`serving on port 5000 ${PORT}`)
+ })
+
+
+
+
+
+
+
+ 
+// app.post("/signup",(req,res)=>{
+//     const Email = req.body.uEmail;
+//     const password = req.body.uPassword;
+
+//     console.log(Email);
+//     console.log(password);
+
+//     res.send("<h1>Signed up</h1>")
+// })
+
+// app.get("/",(req,res)=>{
+//      res.send("<h1>this is node.js </h1>");
+// })
+
+
+
+// app.post("/contact",(req,res)=>{
+//     res.send("<h1>data sent</h1>");
+// })
+
+
+// app.get("/",(req,res)=>{
+//     res.render("index");
+// })
+
